0e903c08e63a63b8b6a0e4964d06d99985ed9575
zhoutao <wb_zhoutao09@meituan.com>
2025-07-09 17:34:02 +0800

    init bookkeeping user service

---
+++ user-starter/src/main/java/com/zt/bookkeeping/user/starter/controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.zt.bookkeeping.user.starter.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Desc:
+ * ------------------------------------
+ * Author:zt@meituan.com
+ * Date:2025/7/9
+ * Time:17:28
+ */
+@RequestMapping("/user/login")
+@RestController
+public class LoginController {
+
+    @PostMapping
+    public Long login(){
+
+
+
+    }
+}
